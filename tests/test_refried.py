ec028b248036cd3ed2a34107509e57201f4cf42a
Sunjay Cauligi <scauligi@eng.ucsd.edu>
2020-11-26 23:00:59 -0800

    bump to 0.3.0

--- tests/test_refried.py
+++ tests/test_refried.py
@@ -6,7 +6,7 @@ from refried import *
 from decimal import Decimal
 
 def test_version():
-    assert refried.__version__ == '0.2.1'
+    assert refried.__version__ == '0.3.0'
 
 class TestRefried(unittest.TestCase):
     def test_halfcents(self):
